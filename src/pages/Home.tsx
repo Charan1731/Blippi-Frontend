4b57b2b2b83960a81bf593cf7532c6c521b603fc
Charan1731 <charan23114@gmail.com>
2025-01-20 21:43:04 +0530

    Fix: Fixed the error of the loading animation of no waller users

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -75,7 +75,9 @@ export default function Home() {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-64">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+        <h1 className='heading text-xl text-blue-600 dark:text-blue-400'>
+          Please Connect to a Metamask wallet and Enable test network and Choose Sepolia as Testnet to view the blogs
+        </h1>
       </div>
     );
   }
